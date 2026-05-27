3a3e670a166a1170493f0517b6397aea37a00e3b
mentallyderangeduser <yeayearemix@gmail.com>
2026-05-27 12:16:49 -0400

    added comments for jq1

--- jq1.java
+++ jq1.java
@@ -4,9 +4,9 @@ public class Main   {
     public static void main(String[] args)    {
         Scanner s = new Scanner(System.in);
 
-        int B = s.nextInt();
-        int T = s.nextInt(); 
-        int P = s.nextInt();
+        int B = s.nextInt();    //number of tickets besa wants to buy
+        int T = s.nextInt();    //total number of tickets for concert
+        int P = s.nextInt();    //number of tickets others have purchased
 
         int determinant = T-(B+P);
 
